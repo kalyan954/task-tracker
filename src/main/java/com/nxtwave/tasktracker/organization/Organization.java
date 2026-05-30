efd2e00062434cacc1733c38945118f35988f265
kalyan954 <kalyankakarlapudi954@gmail.com>
2026-05-30 16:54:23 +0530

    add organization entity with auditing support

---
+++ src/main/java/com/nxtwave/tasktracker/organization/Organization.java
@@ -0,0 +1,22 @@
+package com.nxtwave.tasktracker.organization;
+
+import com.nxtwave.tasktracker.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "organizations")
+public class Organization extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
